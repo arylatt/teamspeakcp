341359cddd8d2975edd4082c3c0f4e93272b4861
Andrew Rylatt <andrew@rylatt.me>
2016-08-01 18:45:28 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
 
 class FrontendController extends Controller
 {
-    public function Index(Request $req) {
-    	return view('fe.index');
+    public function Index(Request $req)
+    {
+        return view('fe.index');
     }
 }
